bc30fc3d4ffe3ead4060dbdf9c8f82b42e1b2edf
santhiya2001 <santhiyar383@gmail.com>
2021-05-27 20:21:01 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,33 @@
+let API_KEY = "10d535d8dad64606479a529972f16544";
+
+
+getWeatherdata = (city) => {
+ const URL = "https://api.openweathermap.org/data/2.5/weather";
+ const FULL_URL = `${URL}?q=${city}&appid=${API_KEY}&units=imperial`;
+ const weatherPromise = fetch(FULL_URL);
+  return weatherPromise.then((response) => {
+   return response.json();
+ })
+}
+
+
+
+searchCity = () => {
+ const city = document.getElementById('city-input').value;
+ getWeatherdata(city)
+ .then((response) => {
+  showWeatherData(response);
+ }).catch((error) => {
+  console.log(error);
+  } )
+ }
+
+
+ showWeatherData = (weatherData) => {
+  document.getElementById('city-name').innerText = weatherData.name;
+  document.getElementById('weather-type').innerText = weatherData.weather[0].main;
+  document.getElementById('temp').innerText = weatherData.main.temp;
+  document.getElementById('min-temp').innerText = weatherData.main.temp_min;
+  document.getElementById('max-temp').innerText = weatherData.main.temp_max;
+
+ }
